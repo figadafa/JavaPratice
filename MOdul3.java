39f49fb3dd1350bdac77385bf73d349c89346e21
figadafa <figabrilliantdafa1@gmail.com>
2025-10-24 13:33:50 +0700

    "Menambahkan output Codelab 1 selesai"

---
+++ MOdul3.java
@@ -0,0 +1,10 @@
+public class MOdul3 {
+        public static void main(String[] args) {
+            //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+            // to see how IntelliJ IDEA suggests fixing it.
+            System.out.println("Hello modul 3");
+            System.out.println("Codelab1 seesai");
+
+
+        }
+}
